c1d0f7de81cea19cc73b92061935d13985ac1ddc
usmanBoi <usmankith@gmail.com>
2025-08-31 00:56:10 +0500

    workflow editor^+

---
+++ src/app/(main)/(pages)/workflows/editor/[editorid]/_components/render-output-accordion.tsx
@@ -0,0 +1,31 @@
+import { ConnectionProviderProps } from "@/src/providers/connections-provider";
+import { EditorState } from "@/src/providers/editor-provider";
+import { useFlowtomicStore } from "@/src/store";
+import React from "react";
+import ContentBasedOnTitle from "./content-based-on-title";
+
+type Props = {
+  state: EditorState;
+  nodeConnection: ConnectionProviderProps;
+};
+
+const RenderOutputAccordion = ({ state, nodeConnection }: Props) => {
+  const {
+    googleFile,
+    setGoogleFile,
+    selectedSlackChannels,
+    setSelectedSlackChannels,
+  } = useFlowtomicStore();
+  return (
+    <ContentBasedOnTitle
+      nodeConnection={nodeConnection}
+      newState={state}
+      file={googleFile}
+      setFile={setGoogleFile}
+      selectedSlackChannels={selectedSlackChannels}
+      setSelectedSlackChannels={setSelectedSlackChannels}
+    />
+  );
+};
+
+export default RenderOutputAccordion;
